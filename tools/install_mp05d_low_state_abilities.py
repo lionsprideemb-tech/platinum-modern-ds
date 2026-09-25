8965e41bfe249b3db5621e9958c0b1235eb285fb
lionsprideemb-tech <lionsprideemb@gmail.com>
2026-09-25 11:55:50 -0500

    Use Platinum sand weather macro in MP05D

--- tools/install_mp05d_low_state_abilities.py
+++ tools/install_mp05d_low_state_abilities.py
@@ -93,14 +93,14 @@ def main() -> None:
 """
     speed_replacement = """        if ((battler1Ability == ABILITY_SWIFT_SWIM && WEATHER_IS_RAIN)
             || (battler1Ability == ABILITY_CHLOROPHYLL && WEATHER_IS_SUN)
-            || (battler1Ability == ABILITY_SAND_RUSH && WEATHER_IS_SANDSTORM)
+            || (battler1Ability == ABILITY_SAND_RUSH && WEATHER_IS_SAND)
             || (battler1Ability == ABILITY_SLUSH_RUSH && WEATHER_IS_HAIL)) {
             battler1Speed *= 2;
         }
 
         if ((battler2Ability == ABILITY_SWIFT_SWIM && WEATHER_IS_RAIN)
             || (battler2Ability == ABILITY_CHLOROPHYLL && WEATHER_IS_SUN)
-            || (battler2Ability == ABILITY_SAND_RUSH && WEATHER_IS_SANDSTORM)
+            || (battler2Ability == ABILITY_SAND_RUSH && WEATHER_IS_SAND)
             || (battler2Ability == ABILITY_SLUSH_RUSH && WEATHER_IS_HAIL)) {
             battler2Speed *= 2;
         }
